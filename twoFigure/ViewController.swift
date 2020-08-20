9239990456d97181b69524dfe12cd256284f9e0a
Juli-Ivanova <julia_2705@icloud.com>
2020-08-20 20:21:03 +0300

    Initial Commit

---
+++ twoFigure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twoFigure
+//
+//  Created by Юлия Иванова on 20.08.2020.
+//  Copyright © 2020 Юлия Иванова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
